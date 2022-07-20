b767bbf3d53aada4162be36584bf826ee1bc5bad
sgujuluva <sanghee04@gmail.com>
2022-07-20 07:54:32 +0200

    useeffect written for navigator the mediadevices

--- src/App.js
+++ src/App.js
@@ -7,12 +7,27 @@ import "./App.css";
 function App() {
   let videoRef = useRef(null);
   let photoRef = useRef(null);
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({video : true})
+    .then(response => {
+     
+      let video = videoRef.current;
+    
+      video.srcObject = response;
+   
+      /* video.play(); */
+    })
+  },[videoRef])
+
+  const handleClick = () => {
+    
+  }
   return (
     <div className="App">
       <h1>Camera App</h1>
-      <button>Take Picture</button>
+      <button onClick={handleClick}>Take Picture</button>
       <video ref = {videoRef}></video>
-      <canvas ref={photoRef}></canvas>
+      <canvas ref={photoRef}></canvas> 
     </div>
   );
 }
