856f66e264258de80462d689000f5ebc08a93087
sgujuluva <sanghee04@gmail.com>
2022-07-20 08:06:35 +0200

    handleclick current video captured and photo drawn in canvas

--- src/App.js
+++ src/App.js
@@ -15,15 +15,24 @@ function App() {
     
       video.srcObject = response;
    
-      /* video.play(); */
+       video.play();
     })
   },[videoRef])
 
   const handleClick = () => {
+    //capturing the current video
     let video = videoRef.current;
+    //capturing the current photo from the current video
     let photo = photoRef.current;
-    const width = 400;
-    const height = 500;
+    //setting height and width to draw in canvas
+    const width = 500;
+    const height = 200;
+    //setting height and width for the photo captured
+    photo.width = width;
+    photo.height = height;
+    //drawing in canvas
+    let ctx = photo.getContext("2d");
+    ctx.drawImage(video,0,0,width,height)
   }
   return (
     <div className="App">
