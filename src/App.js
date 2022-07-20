cd619c804030e1dabe75b2bf7900851aff69566c
sgujuluva <sanghee04@gmail.com>
2022-07-20 07:37:36 +0200

    photo ref taken from useref from canvas element

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 //react lib
-import {useEffect, useRef} from "react";
-import ImageGallery from "react-image-gallery"
+import { useEffect, useRef } from "react";
+import ImageGallery from "react-image-gallery";
 //styles
-import './App.css';
+import "./App.css";
 
 function App() {
   let videoRef = useRef(null);
+  let photoRef = useRef(null);
   return (
     <div className="App">
       <h1>Camera App</h1>
-      <video ref = {videoRef}></video>
       <button>Take Picture</button>
+      <video ref = {videoRef}></video>
+      <canvas ref={photoRef}></canvas>
     </div>
   );
 }
