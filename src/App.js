acd12ef096aefc29699b57011fd9d3f0d0d8c267
sgujuluva <sanghee04@gmail.com>
2022-07-20 07:35:41 +0200

    video tag and button added to take pic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+//react lib
+import {useEffect, useRef} from "react";
+import ImageGallery from "react-image-gallery"
 //styles
 import './App.css';
 
 function App() {
+  let videoRef = useRef(null);
   return (
     <div className="App">
       <h1>Camera App</h1>
+      <video ref = {videoRef}></video>
+      <button>Take Picture</button>
     </div>
   );
 }
