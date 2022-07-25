b000963bbae1f2b797c7d3fd7e7c9ae5a8cf2461
sgujuluva <sanghee04@gmail.com>
2022-07-25 19:20:35 +0200

    boolean vale added for the load image icons

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 //react lib
-import { useEffect, useRef,useState } from "react";
+import { useEffect, useRef, useState } from "react";
 //import {BsFillCameraFill} from "react-icons/bs"
-import resetImg from "./images/reset.png"
-import cameraImg from "./images/camera-ani.gif"
-import cameraStill from "./images/camera-still.png"
-import uploadImg from "./images/upload.gif"
-import downloadImg from "./images/download.gif"
- 
+import resetImg from "./images/reset.png";
+import cameraImg from "./images/camera-ani.gif";
+import cameraStill from "./images/camera-still.png";
+import uploadImg from "./images/upload.gif";
+import downloadImg from "./images/download.gif";
+
 //styles
-import "./App.css";
+ import "./App.css"; 
 
 function App() {
-  const [camImg,setCamImg] = useState(cameraStill)
-  const [loadImg, setLoadImg] = useState("")
-const [base64Img, setBase64Img] = useState("");
+  const [camImg, setCamImg] = useState(cameraStill);
+  const [loadImg, setLoadImg] = useState(false);
+  const [base64Img, setBase64Img] = useState("");
   let videoRef = useRef(null);
   let photoRef = useRef(null);
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({video : true})
-    .then(response => {
-     //capturing the current video using useRef
+    navigator.mediaDevices.getUserMedia({ video: true }).then((response) => {
+      //capturing the current video using useRef
       let video = videoRef.current;
-    // intialising the response object to video sourceobj
+      // intialising the response object to video sourceobj
       video.srcObject = response;
-   // video streaming (auto play)
-         video.play();  
-    })
-  },[videoRef])
+      // video streaming (auto play)
+      video.play();
+    });
+  }, [videoRef]);
 
   const handleClick = () => {
     //while clicking cameraani pic to be shown from still to ani
-    setCamImg(cameraImg );
+    setCamImg(cameraImg);
+
+    setLoadImg(true);
+    
     //capturing the current video
     let video = videoRef.current;
     //capturing the current photo from the current video
@@ -39,45 +41,56 @@ const [base64Img, setBase64Img] = useState("");
     const width = video.offsetWidth;
     const height = video.offsetHeight;
     //setting height and width for the photo captured
-    /* photo.width = width;
-    photo.height = height; */
+    photo.width = width;
+    photo.height = height;
     //drawing in canvas
     let ctx = photo.getContext("2d");
-    ctx.drawImage(video,0,0,width,height);
+    ctx.drawImage(video, 0, 0, width, height);
     //to bring the animated camer to normal camera---setting to normal
-    setTimeout(function(){
+    setTimeout(function () {
       setCamImg(cameraStill);
-    },1500)
-  }
+    }, 1500);
+  };
   const handleReset = () => {
-    setLoadImg("") // --------------not working--------
+    setLoadImg(false); 
     let photo = photoRef.current;
     let ctx = photo.getContext("2d");
-    ctx.clearRect(0,0,photo.width,photo.height); 
-    setTimeout(function(){// --------------not working--------
-      setLoadImg(uploadImg);
-    },1500) 
-  }
+    ctx.clearRect(0, 0, photo.width, photo.height);
+    
+  };
 
   const handleUpload = () => {
     let photo = photoRef.current;
     const convert2Base64 = photo.toDataURL("image/jeg");
     setBase64Img(convert2Base64);
-    
-      }
+  };
   return (
     <div className="App">
-      <h1>Camera App</h1>      
-      <video ref = {videoRef}></video>
-      <div className = "button">
-      <img src={camImg} alt = "camera img" className = "takePic" onClick={handleClick}/>
-      <img src={resetImg} alt = "reset img" className = "reset" onClick = {handleReset}/>
+      <h1>Camera App</h1>
+      <video ref={videoRef}></video>
+      <div className="button">
+        <img
+          src={camImg}
+          alt="camera img"
+          className="takePic"
+          onClick={handleClick}
+        />
+        <img
+          src={resetImg}
+          alt="reset img"
+          className="reset"
+          onClick={handleReset}
+        />
       </div>
-      
-      <canvas ref={photoRef}></canvas> 
-      <div className = "load">
-        <button className = "upload" onClick = {handleUpload}><img src={uploadImg}/></button>
-        <button className = "download"><img src={downloadImg}/></button>
+
+      <canvas ref={photoRef}></canvas>
+      <div className="load">
+        {loadImg ? (<div><button className="upload" onClick={handleUpload}>
+          <img src={uploadImg} />
+        </button>
+        <button className="download">
+          <img src={downloadImg} />
+        </button></div>) : ""}
       </div>
     </div>
   );
